9c899bb8a4613c7845c731ac36b34857856d8128
Brandon Paulsen <pauls658@d.umn.edu>
2017-11-29 18:39:30 -0800

    fixed small bug with individual marker handling. now going on to pair handling

--- php_wrappers/explode.php
+++ php_wrappers/explode.php
@@ -39,16 +39,15 @@ function debreach_wrap_explode2($delim, $string, $count = PHP_INT_MAX) {
 	// do the exploding...
 	$string = str_replace(array($open_marker, $close_marker), "", $string);
 	$parts = explode($delim, $string, $count);
+
 	// put the markers back in
 	$part_i = 0; // the part we are working on
 	$part_offset = 0; // the amount of string in the previously processed parts
-	//$delim_offset = 0; // change in position due to removing doelim
 	$marker_offset = 0; // the change in position due to adding in markers
 	$open = true;
 	print_r($positions);
 	for ($i = 0; $i < count($positions); $i++) {
 		$index_in_part = $positions[$i] - $part_offset + $marker_offset;
-
 		if ($index_in_part < strlen($parts[$part_i])) {
 			// this marker belongs in this part
 			if ($open) {
@@ -62,19 +61,26 @@ function debreach_wrap_explode2($delim, $string, $count = PHP_INT_MAX) {
 				$open = true;
 			}
 		} else {
-			$part_offset += strlen($parts[$part_i]) + strlen($delim);
+			$part_offset += strlen($parts[$part_i]) + strlen($delim) - $marker_offset;
 			$part_i++;
-			while ($index_in_part - $part_offset >= strlen($parts[$part_i])) {
+			$marker_offset = 0;
+			$index_in_part = $positions[$i] - $part_offset;
+			while ($index_in_part >= strlen($parts[$part_i])) {
 				$part_offset += strlen($parts[$part_i]) + strlen($delim);
 				$part_i++;
+				$index_in_part = $positions[$i] - $part_offset;
 			}
-			$marker_offset = 0;
+
+			$index_in_part = max($index_in_part, 0);
+
 			if ($open) {
+				echo "else open: $index_in_part, " . strlen($parts[$part_i]) . "\n";
 				$parts[$part_i] = substr_replace($parts[$part_i], $open_marker, $index_in_part, 0);
 				$marker_offset += strlen($open_marker);
 				$open = false;
 			}
 			else {
+				echo "else close: $index_in_part, " . strlen($parts[$part_i]) . "\n";
 				$parts[$part_i] = substr_replace($parts[$part_i], $close_marker, $index_in_part, 0);
 				$marker_offset += strlen($close_marker);
 				$open = true;
