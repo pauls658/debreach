cac798ebf7a0144d718cabf83ea7efb4092398df
Brandon Paulsen <bpaulsen@usc.edu>
2019-01-21 14:26:15 -0800

    small updates to these

--- init.php
+++ init.php
@@ -1,5 +1,10 @@
 <?php
-require_once 'debreach.php';
-$rustart = getrusage();
+//require_once 'debreach.php';
 $dbr_script_start = microtime(true);
-?>
+
+function shutdown_dbr() {
+	global $dbr_script_start;
+	file_put_contents("/home/brandon/php_res/exec_times", (microtime(true) - $dbr_script_start) . "\n", FILE_APPEND);
+}
+
+register_shutdown_function("shutdown_dbr");
